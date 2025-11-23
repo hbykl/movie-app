09db2bd3fed35ee39746efdb31ead3cdf13147d2
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2025-11-23 22:39:00 +0300

    StarRating Component

---
+++ src/StarRating.jsx
@@ -0,0 +1,23 @@
+const containerStyle = {
+  display: "flex",
+  gap: "1rem",
+};
+const itemContainerStyle = {
+  display: "flex",
+  gap: ".2rem",
+};
+const textStyle = {
+  margin: "0",
+};
+export default function StarRating({ maxRating = 5 }) {
+  return (
+    <div style={containerStyle}>
+      <div style={itemContainerStyle}>
+        {Array.from({ length: maxRating }, (val, i) => (
+          <i className="bi bi-star" key={i}></i>
+        ))}
+      </div>
+      <p style={textStyle}>4</p>
+    </div>
+  );
+}
