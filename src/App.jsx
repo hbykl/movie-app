ce2029ce651856403c95403ebf3dec1766ae2aef
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2025-11-23 03:00:58 +0300

    fix the design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
 
 function App() {
   return (
     <>
+      <nav className="bg-primary text-white p-2">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-4">Movie App</div>
+            <div className="col-4">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Film arayın..."
+              />
+            </div>
+            <div className="col-4 text-end">
+              <strong>5</strong> kayıt bulundu.
+            </div>
+          </div>
+        </div>
+      </nav>
+      <main className="container">
+        <div className="row mt-2">
+          <div className="col-md-9">Movie List</div>
+          <div className="col-md-3">Selected Movie List</div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
