c9669a65f98bfb3945b1983780f30f3f090b9bc3
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2025-11-25 23:54:16 +0300

    async and await

--- src/App.jsx
+++ src/App.jsx
@@ -1,132 +1,5 @@
 import { useEffect } from "react";
 import { useState } from "react";
-const movie_list = [
-  {
-    Id: "769",
-    Title: "Goodfellas",
-    Year: "1990",
-    Poster:
-      "https://image.tmdb.org/t/p/original/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
-  },
-  {
-    Id: "120",
-    Title: "The Lord of the Rings",
-    Year: "2001",
-    Poster:
-      "https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
-  },
-  {
-    Id: "27205",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://image.tmdb.org/t/p/original/1jsZT bVsrqSqzGWeep2B1QiDKuh.jpg",
-  },
-  {
-    Id: "238",
-    Title: "The Godfather",
-    Year: "1972",
-    Poster:
-      "https://image.tmdb.org/t/p/original/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
-  },
-  {
-    Id: "680",
-    Title: "Pulp Fiction",
-    Year: "1994",
-    Poster:
-      "https://image.tmdb.org/t/p/original/dM2w364MScsjFf8pfMbaWUcWrR.jpg",
-  },
-  {
-    Id: "155",
-    Title: "The Dark Knight",
-    Year: "2008",
-    Poster:
-      "https://image.tmdb.org/t/p/original/1hRoyzDtpgMU7Dz4JF22RANzQO7.jpg",
-  },
-  {
-    Id: "550",
-    Title: "Fight Club",
-    Year: "1999",
-    Poster:
-      "https://image.tmdb.org/t/p/original/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg",
-  },
-  {
-    Id: "13",
-    Title: "Forrest Gump",
-    Year: "1994",
-    Poster:
-      "https://image.tmdb.org/t/p/original/saHP97rTPS5eLmrLQEcANmKrsFl.jpg",
-  },
-  {
-    Id: "807",
-    Title: "Se7en",
-    Year: "1995",
-    Poster:
-      "https://image.tmdb.org/t/p/original/6yoghtyTpznpBik8EngEmJskVUO.jpg",
-  },
-  {
-    Id: "1891",
-    Title: "The Empire Strikes Back",
-    Year: "1980",
-    Poster:
-      "https://image.tmdb.org/t/p/original/7BuH8itoSrLExs2YZSsM01Qk2no.jpg",
-  },
-  {
-    Id: "603",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://image.tmdb.org/t/p/original/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg",
-  },
-  {
-    Id: "12444",
-    Title: "Harry Potter and the Deathly Hallows: Part 2",
-    Year: "2011",
-    Poster:
-      "https://image.tmdb.org/t/p/original/da22ZBmrDOXOCDRvr8Gic8ldhv.jpg",
-  },
-  {
-    Id: "8077",
-    Title: "The Truman Show",
-    Year: "1998",
-    Poster:
-      "https://image.tmdb.org/t/p/original/yJrOYUyjkjFTqfEGxWpYH28zbKA.jpg",
-  },
-  {
-    Id: "7698",
-    Title: "The Green Mile",
-    Year: "1999",
-    Poster:
-      "https://image.tmdb.org/t/p/original/8VG8fDNiy50H4FedGwdSVUPoaJe.jpg",
-  },
-  {
-    Id: "1572",
-    Title: "Die Hard",
-    Year: "1988",
-    Poster:
-      "https://image.tmdb.org/t/p/original/1Nq3GEX96A1Fzfb5jBqFaQZ02p.jpg",
-  },
-];
-const selected_movie_list = [
-  {
-    Id: "769",
-    Title: "Goodfellas",
-    Year: "1990",
-    Poster:
-      "https://image.tmdb.org/t/p/original/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
-    Duration: 120,
-    Rating: 8.5,
-  },
-  {
-    Id: "120",
-    Title: "The Lord of the Rings",
-    Year: "2001",
-    Poster:
-      "https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
-    Duration: 125,
-    Rating: 8.1,
-  },
-];
 
 const api_key = "242fc01a5db5c09e93d411a0770051e3";
 const query = "First";
@@ -135,12 +8,16 @@ export default function App() {
   const [selectedMovies, setSelectedMovies] = useState([]);
   useEffect(function () //first render
   {
-    fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}`
-    )
-      .then((res) => res.json())
-      .then((res) => setMovies(res.results));
+    async function getMovies() {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}`
+      );
+      const result = await data.json();
+      setMovies(result.results);
+    }
+    getMovies();
   }, []);
+
   const handleClickMovie = (movie, movieId) => {
     const exist = selectedMovies.some((m) => m.id == movieId);
     !exist && setSelectedMovies([...selectedMovies, movie]);
